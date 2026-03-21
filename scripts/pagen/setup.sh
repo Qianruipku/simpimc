d7e92e805f6f78072b2f6cb76ca70a33bab5083c
Qianruipku <terry_liu@pku.edu.cn>
2026-03-21 16:10:50 +0000

    Fix setup.sh in scripts/pagen

--- scripts/pagen/setup.sh
+++ scripts/pagen/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
-
 cd davidSquarer && ./machconfig && make && cd ..
 cd ilkkaSquarer && make && cd ..
-cd exact && f2py -c -m coulcc coulcc36-f90.f90 && cd ..
+cd exact && f2py -c coulcc.pyf coulcc36-f90.f90 --f90flags="-ffixed-form" --f77flags="-ffixed-form" && cd ..
